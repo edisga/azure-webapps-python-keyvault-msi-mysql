b76b80efe19007c201dc2b9b600f672ed531db69
Edison Garcia <egachi@live.com>
2021-02-05 13:44:51 -0600

    Updating

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 import mysql.connector
 from mysql.connector import errorcode
 
-from azure.identity import AppServiceCredential
+from azure.identity import ManagedIdentityCredential
 from azure.keyvault.secrets import SecretClient
 import sys, os
 
@@ -19,7 +19,8 @@ vault_name = os.environ.get('VAULT_NAME')
 
 def Connect():
     try:
-        credential = AppServiceCredential()
+        credential = ManagedIdentityCredential()
+        print (credential.get_token())
         vault_url = "https://{}.vault.azure.net".format(vault_name)
         secret_client = SecretClient(vault_url=vault_url, credential=credential)
         retrieved_secret = secret_client.get_secret("mysql")
